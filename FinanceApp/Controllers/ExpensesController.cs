7ce883616465e9c2c129c1ae3d0d507ae9701d6e
Ford Henley <info@fordswebhelp.com>
2025-10-31 12:31:43 -0400

    Migrated DB and started MVC setup

---
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
